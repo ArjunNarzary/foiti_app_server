5854d58ba71a05e31f1c77ffab4e315d6f9a3c28
ArjunNzy <arjunarzary@gmail.com>
2022-09-06 21:49:50 +0530

    Bug fix in inappnotification

--- utils/sendInAppNotification.js
+++ utils/sendInAppNotification.js
@@ -117,7 +117,6 @@ async function sendPushNotification(receiver_id, message) {
     const expo = new Expo();
     const messages = [];
     const user = await Notification.findOne({ user: receiver_id }).populate('user', 'expoToken');
-    console.log(user);
     if(!user) return;
     if(!Expo.isExpoPushToken(user.user.expoToken)) return;
 
